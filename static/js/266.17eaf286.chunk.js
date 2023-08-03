52103df39030748d725d65a8f03e32b93d6ed626
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-03 20:51:49 +0000

    Deploying to gh-pages from @ Alekcandrovich/goit-react-hw-08-phonebook@4f355872b506601f8d7d4e1c4f1c47389b56ce72 🚀

---
+++ static/js/266.17eaf286.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var c=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:700,fontSize:52,textAlign:"center",color:"#057ed4"}};function o(){return(0,c.jsx)("div",{style:i.container,children:(0,c.jsx)("h1",{style:i.title,children:"Welcome to Phonebook"})})}}}]);
+//# sourceMappingURL=266.17eaf286.chunk.js.map
