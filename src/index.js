99370056ed933ed1be4ff2b1158629019a3803fc
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-08-03 19:22:46 +0300

    добавил стили

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import 'modern-normalize';
-import './styles.css';
+import 'index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
